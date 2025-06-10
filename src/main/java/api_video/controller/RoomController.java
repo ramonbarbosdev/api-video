097c7615b7582aa98607cc081b08696b58fb82ea
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2025-06-10 18:42:39 -0300

    Adiciona métodos de cadastro e exclusão no RoomController e completa os métodos de acesso no modelo Room

--- src/main/java/api_video/controller/RoomController.java
+++ src/main/java/api_video/controller/RoomController.java
@@ -1,11 +1,16 @@
 package api_video.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +30,30 @@ public class RoomController {
          List<Room> rooms = repository.findAll();
         return new ResponseEntity<>(rooms, HttpStatus.OK);
     }
+
+    @PostMapping(value = "/", produces = "application/json")
+    public ResponseEntity<Room> cadastrar(@RequestBody Room room)
+    {
+
+        Room objeto = repository.save(room);
+        return new ResponseEntity<>(objeto, HttpStatus.OK);
+    }
+
+
+     @DeleteMapping(value = "/{id}", produces = "application/json" )
+	public ResponseEntity<?> delete (@PathVariable("id") String id) throws Exception
+	{
+
+        if (!repository.existsById(id))
+        {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", "Registro não encontrado com o ID: " + id));
+        }
+
+        repository.deleteById( id);
+			
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("message","Registro deletado!"));
+
+	}
 
     
 }
--- src/main/java/api_video/model/Room.java
+++ src/main/java/api_video/model/Room.java
@@ -11,4 +11,37 @@ public class Room {
     private String nm_room;
     private String ds_room;
     private String nu_capacidade;
+
+    public String getId_room() {
+        return id_room;
+    }
+    
+    public void setId_room(String id_room) {
+        this.id_room = id_room;
+    }
+
+    public String getNm_room() {
+        return nm_room;
+    }
+
+    public void setNm_room(String nm_room) {
+        this.nm_room = nm_room;
+    }
+
+    public String getDs_room() {
+        return ds_room;
+    }
+
+    public void setDs_room(String ds_room) {
+        this.ds_room = ds_room;
+    }
+
+    public String getNu_capacidade() {
+        return nu_capacidade;
+    }
+
+    public void setNu_capacidade(String nu_capacidade) {
+        this.nu_capacidade = nu_capacidade;
+    }
+    
 }
