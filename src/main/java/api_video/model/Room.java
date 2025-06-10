9f27d0172ac2e62cacb0682c93605cb614a67e8a
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2025-06-10 19:34:51 -0300

    Ajusta tipo de nu_capacidade para int e adiciona novos atributos tp_status, nu_ocupante e imageUrl na classe Room

--- src/main/java/api_video/model/Room.java
+++ src/main/java/api_video/model/Room.java
@@ -13,7 +13,10 @@ public class Room {
     private long id_room;
     private String nm_room;
     private String ds_room;
-    private String nu_capacidade;
+    private int nu_capacidade;
+    private String tp_status;
+    private int nu_ocupante;
+    private String imageUrl;
 
     public long getId_room() {
         return id_room;
@@ -39,12 +42,35 @@ public class Room {
         this.ds_room = ds_room;
     }
 
-    public String getNu_capacidade() {
+    public int getNu_capacidade() {
         return nu_capacidade;
     }
 
-    public void setNu_capacidade(String nu_capacidade) {
+    public void setNu_capacidade(int nu_capacidade) {
         this.nu_capacidade = nu_capacidade;
+    }
+
+    public String getTp_status() {
+        return tp_status;
+    }
+
+    public void setTp_status(String tp_status) {
+        this.tp_status = tp_status;
+    }
+
+    public int getNu_ocupante() {
+        return nu_ocupante;
+    }
+
+    public void setNu_ocupante(int nu_ocupante) {
+        this.nu_ocupante = nu_ocupante;
+    }
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
     
 }
